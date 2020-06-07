9faf70809d4c4f47eca3efaebee79df88d82da8c
Bruno Alves <alves.bruno945@gmail.com>
2020-06-07 15:23:08 -0300

    OperatorsSwift
    Using operators in Swift

---
+++ Swift - Operadores.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Atribuicao "="
+var name = "Bruno"
+print(name)
+
+// Operadores: +,-,*,/
+var (number, number1) = (10,20)
+let result = number + number1
+print(result)
+
+// Operadores compostos
+number += number1
+print(number)
+
+// Operador Ternario - Ele atua em tres opcoes
+var nota = 7.00
+var resultadoNota = (nota >= 7.00) ? "Aprovado" : "Reprovado"
